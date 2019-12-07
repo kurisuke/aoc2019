564ed748759c284eb1b39fc6b3d7e0ef5c5a764b
Peter Helbing <peter@abulafia.org>
2019-12-07 11:26:36 +0100

    Day 04 (part 2)

--- day04/index.ts
+++ day04/index.ts
@@ -10,6 +10,28 @@ function checkAdj(digits: number[]) {
     return false;
 }
 
+// there must be one sequence of same digits with _exactly_ length 2
+function checkAdj2(digits: number[]) {
+    let len = 1;
+    for (let j = 0; j < digits.length - 1; j++) {
+        if (digits[j] === digits[j + 1]) {
+            len++;
+        } else { // end of sequence, check length
+            if (len === 2) {
+                return true;
+            }
+            len = 1;
+        }
+    }
+
+    // check again at end of the loop
+    if (len === 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function checkAsc(digits: number[]) {
     for (let j = 0; j < digits.length - 1; j++) {
         if (digits[j + 1] < digits[j]) {
@@ -23,7 +45,7 @@ let count = 0;
 
 for (let i = rangeMin; i <= rangeMax; i++) {
     const digits = String(i).split("").map((c) => Number(c));
-    if (checkAdj(digits) && checkAsc(digits)) {
+    if (checkAdj2(digits) && checkAsc(digits)) {
         count++;
     }
 }
