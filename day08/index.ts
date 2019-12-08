23f39e82a837f3ab72762fc74f59c61638aa823f
Peter Helbing <peter@abulafia.org>
2019-12-08 11:15:50 +0100

    Day 08 (part 2)

--- day08/index.ts
+++ day08/index.ts
@@ -10,26 +10,57 @@ type Histogram = { [id: string]: number; };
 function createHisto(ls: string) {
     const h: Histogram = {};
 
-    // initialize fields
-    for (const c of "0123456789".split("")) {
-        h[c] = 0;
-    }
-
     // count occurences
     for (const c of ls) {
-        h[c]++;
+        h[c] = h[c] === undefined ? 0 : h[c] + 1;
     }
 
     return h;
 }
 
 readFile("day08.input", "utf8", (error, data) => {
-    const layers: Histogram[] = [];
 
+    // part 1
+    const layers: Histogram[] = [];
     for (let i = 0; i < data.length; i += layerSize) {
         layers.push(createHisto(data.substr(i, layerSize)));
     }
 
     const minIdx = layers.reduce((lowest, next, idx) => next["0"] < layers[lowest]["0"] ? idx : lowest, 0);
     console.log(layers[minIdx]["1"] * layers[minIdx]["2"]);
+
+    // part 2
+    const pixelLayers: string[][] = [];
+    for (let i = 0; i < layerSize; i++) {
+        pixelLayers.push([]);
+    }
+
+    // split input per pixel: store an array of layer values for each pixel
+    data.split("").forEach((c, idx) => pixelLayers[idx % layerSize].push(c));
+
+    const pixels = pixelLayers.map((pixel) => {
+        let p = "2";
+        // iterate over layer values from top layer, until a non-transparent pixel is found
+        for (const l of pixel) {
+            if (l !== "2") {
+                p = l;
+                break;
+            }
+        }
+        return p;
+    });
+
+    // convert for pretty printing
+    // black / white inverted for better readability on black terminal
+    const printTransform: { [key: string]: string; } = {
+        0: " ", // black
+        1: "#", // white
+        2: ".",
+    };
+    const printPixels = pixels.map((c) => printTransform[Number(c)]);
+
+    // pretty print line per line
+    for (let i = 0; i < printPixels.length; i += IMG_WIDTH) {
+        console.log(printPixels.slice(i, i + IMG_WIDTH).join(""));
+    }
 });
