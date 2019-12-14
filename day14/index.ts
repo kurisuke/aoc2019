aa657df82f42988ba2793ad3b7844d6e4147b2a6
Peter Helbing <peter@abulafia.org>
2019-12-14 16:24:19 +0100

    Day 14 (part 1)

---
+++ day14/index.ts
@@ -0,0 +1,74 @@
+import { readFile } from "fs";
+
+interface Reactant {
+    name: string;
+    qty: number;
+}
+
+interface Reaction {
+    inputs: Reactant[];
+    productQty: number;
+}
+
+type Reactions = {[productName: string]: Reaction};
+type Stockpile = {[productName: string]: number};
+
+function parseReactions(input: string) {
+    const reactions: Reactions = {};
+    for (const line of input.split("\n")) {
+        const inputStr = line.split("=>")[0]!.trim();
+        const outputStr = line.split("=>")[1]!.trim();
+
+        const inputs: Reactant[] = inputStr.split(",").map((chemStr) => {
+            const qty = Number(chemStr.trim().split(" ")[0]!);
+            const name = chemStr.trim().split(" ")[1]!;
+            return {name, qty};
+        });
+
+        const productQty = Number(outputStr.trim().split(" ")[0]!);
+        const productName = outputStr.trim().split(" ")[1]!;
+
+        reactions[productName] = {inputs, productQty};
+    }
+
+    return reactions;
+}
+
+function produce(reactions: Reactions, stockpile: Stockpile, required: Reactant) {
+    // first special case: ore
+    if (required.name === "ORE") {
+        return required.qty;
+    }
+
+    // first use the stuff we have on stock
+    if (stockpile[required.name] === undefined) {
+        stockpile[required.name] = 0;
+    }
+    const reuse = Math.min(required.qty, stockpile[required.name]);
+    const netQty = required.qty - reuse;
+    stockpile[required.name] -= reuse;
+
+    // if not, we have to produce a certain net quantity (that we cannot take from the stockpile)
+    const reaction = reactions[required.name]!;
+    const eqFactor = Math.ceil(netQty / reaction.productQty);
+
+    // now we need to produce our requirements
+    let ore = 0;
+    for (const input of reaction.inputs) {
+        const inputReq = eqFactor * input.qty;
+        ore += produce(reactions, stockpile, {name: input.name, qty: inputReq});
+    }
+
+    // put excess on the stockpile
+    const excess = eqFactor * reaction.productQty - netQty;
+    stockpile[required.name] += excess;
+
+    return ore;
+}
+
+readFile("day14.input", "utf8", (error, data) => {
+    const reactions = parseReactions(data.toString());
+
+    const stockpile: Stockpile = {};
+    console.log(produce(reactions, stockpile, {name: "FUEL", qty: 1}));
+});
