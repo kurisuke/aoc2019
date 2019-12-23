6dfc95cad5b26dba19adadfdccea85c84349de57
Peter Helbing <peter@abulafia.org>
2019-12-23 12:54:41 +0100

    Day 19 (part 2)

--- day19/index.ts
+++ day19/index.ts
@@ -2,21 +2,56 @@ import { readFile } from "fs";
 
 import { Intcode } from "../common/intcode";
 
-function part1(intcodeProg: bigint[]) {
+function isAffected(intcodeProg: bigint[], x: number, y: number) {
     const computer = new Intcode(intcodeProg);
+    computer.writeInp(BigInt(x));
+    computer.writeInp(BigInt(y));
 
+    computer.run();
+
+    return computer.readOutp()! === 1n;
+}
+
+function canFit(intcodeProg: bigint[], x: number, y: number) {
+    return isAffected(intcodeProg, x, y) === true &&
+        isAffected(intcodeProg, x + 99, y) === true &&
+        isAffected(intcodeProg, x, y + 99) === true &&
+        isAffected(intcodeProg, x + 99, y + 99) === true;
+}
+
+function dist(p: number[]) {
+    return p[0]! * p[0]! + p[1]! * p[1]!;
+}
+
+function part2(intcodeProg: bigint[]) {
+    const fitList = [];
+
+    let w = 256;
+    let [xMin, xMax] = [0, 2048];
+    let [yMin, yMax] = [0, 2048];
+    let minPoint = [-1, -1];
+
+    while (w >= 1) {
+        for (let x = xMin; x <= xMax; x += w) {
+            for (let y = yMin; y <= yMax; y += w) {
+                if (canFit(intcodeProg, x, y)) {
+                    fitList.push([x, y]);
+                }
+            }
+        }
+        minPoint = fitList.reduce((a, p) => dist(a) < dist(p) ? a : p, [Infinity, Infinity]);
+        [xMin, xMax] = [minPoint[0] - 4 * w, minPoint[0]];
+        [yMin, yMax] = [minPoint[1] - 4 * w, minPoint[1]];
+        w /= 4;
+    }
+    console.log(minPoint[0] * 10000 + minPoint[1]);
+}
+
+function part1(intcodeProg: bigint[]) {
     let numAffected = 0;
     for (let x = 0; x < 50; x++) {
         for (let y = 0; y < 50; y++) {
-            computer.reset(intcodeProg);
-
-            computer.writeInp(BigInt(x));
-            computer.writeInp(BigInt(y));
-
-            computer.run();
-
-            const ret = computer.readOutp();
-            if (ret === 1n) {
+            if (isAffected(intcodeProg, x, y)) {
                 numAffected++;
             }
         }
@@ -31,4 +66,5 @@ readFile("day19.input", "utf8", (_, data) => {
     });
 
     part1(intcodeProg);
+    part2(intcodeProg);
 });
