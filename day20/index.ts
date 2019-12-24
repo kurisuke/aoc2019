790c6e4ac433c7edd4cd1464048d98e221f0ffbc
Peter Helbing <peter@abulafia.org>
2019-12-24 11:54:07 +0100

    Day 20 (part 2)

--- day20/index.ts
+++ day20/index.ts
@@ -64,7 +64,6 @@ function findAllLabels(grid: Grid) {
                         const posVec2 = new Vec2D(x, y + 1);
                         labelFields.add(posVec2.asStr());
                         id += grid[y + 1][x];
-                        id = id.split("").sort().join("");
                         if (grid[y + 2] && grid[y + 2][x] && grid[y + 2][x] === ".") {
                             const teleportField = new Vec2D(x, y + 2);
                             labels.push({ id, pos: [posVec, posVec2], isInner, teleportField });
@@ -79,7 +78,6 @@ function findAllLabels(grid: Grid) {
                         const posVec2 = new Vec2D(x + 1, y);
                         labelFields.add(posVec2.asStr());
                         id += grid[y][x + 1];
-                        id = id.split("").sort().join("");
                         if (grid[y][x + 2] && grid[y][x + 2] === ".") {
                             const teleportField = new Vec2D(x + 2, y);
                             labels.push({ id, pos: [posVec, posVec2], isInner, teleportField });
@@ -110,21 +108,20 @@ function canTeleport(labels: Label[], field: Vec3D): undefined | [string, Vec3D]
     const fieldXy = new Vec2D(field.x, field.y);
     for (const label of labels) {
         if (fieldXy.asStr() === label.teleportField.asStr()) {
-            for (const labelOther of labels) {
-                if (labelOther.id === label.id && labelOther.teleportField.asStr() !== label.teleportField.asStr()) {
-                    let newLevel = field.z;
-                    if (label.isInner) {
-                        newLevel++;
+            const labelOther = getOtherLabel(labels, label);
+            if (labelOther !== undefined) {
+                let newLevel = field.z;
+                if (label.isInner) {
+                    newLevel++;
+                } else {
+                    if (field.z === 0) {
+                        return undefined;
                     } else {
-                        if (field.z === 0) {
-                            return undefined;
-                        } else {
-                            newLevel--;
-                        }
+                        newLevel--;
                     }
-                    const newField = new Vec3D(labelOther.teleportField.x, labelOther.teleportField.y, newLevel);
-                    return [label.id, newField];
                 }
+                const newField = new Vec3D(labelOther.teleportField.x, labelOther.teleportField.y, newLevel);
+                return [label.id, newField];
             }
         }
     }
@@ -139,10 +136,6 @@ function findPath(grid: Grid, allLabels: Label[], startPoint: Vec3D, endPoint: V
     let steps = 0;
 
     while (frontier.size > 0) {
-        // console.log(steps, Array.from(frontier.values()).join("|"));
-        if (steps === 10000) {
-            break;
-        }
         const newFrontier: Set<string> = new Set();
 
         for (const fieldStr of Array.from(frontier.values())) {
@@ -174,7 +167,6 @@ function findPath(grid: Grid, allLabels: Label[], startPoint: Vec3D, endPoint: V
                 const [id, teleportField] = res;
                 const teleportStr = teleportField.asStr();
                 if (!frontier.has(teleportStr) && !visited.has(teleportStr)) {
-                    console.log("teleport:", id, fieldStr, teleportStr);
                     newFrontier.add(teleportStr);
                 }
             }
@@ -186,7 +178,7 @@ function findPath(grid: Grid, allLabels: Label[], startPoint: Vec3D, endPoint: V
     }
 }
 
-readFile("day20.test3", "utf8", (_, data) => {
+readFile("day20.input", "utf8", (_, data) => {
     // read input into grid[y][x]
     const grid: Grid = [];
     for (const line of data.split("\n")) {
@@ -217,8 +209,6 @@ readFile("day20.test3", "utf8", (_, data) => {
 
     const startPoint = new Vec3D(startPointNoZ.x, startPointNoZ.y, 0);
     const endPoint = new Vec3D(endPointNoZ.x, endPointNoZ.y, 0);
-
-    console.log("start:", startPoint.x, startPoint.y, endPoint.x, endPoint.y);
 
     console.log(findPath(grid, allLabels, startPoint, endPoint));
 });
